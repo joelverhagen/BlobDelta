8e705313d1d5fb5d8e79fb7bddc37b74221cf4eb
Joel Verhagen <joel.verhagen@gmail.com>
2018-12-18 15:09:17 -0800

    Finish functional tests for BlobComparisonEnumerable

--- test/Knapcode.BlobDelta.Test/Functional/BlobComparisonEnumerableFacts.cs
+++ test/Knapcode.BlobDelta.Test/Functional/BlobComparisonEnumerableFacts.cs
@@ -28,6 +28,204 @@ namespace Knapcode.BlobDelta.Test.Functional
             }
         }
 
+        public class ReturnsSamesComparisonsForEqualContainers : Test
+        {
+            public ReturnsSamesComparisonsForEqualContainers(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                var blobNames = new[] { "a", "b", "c" };
+                await CreateLeftBlockBlobsAsync("a", "0", "b", "1", "c", "2");
+                await CreateRightBlockBlobsAsync("a", "0", "b", "1", "c", "2");
+
+                var output = await target.ToListAsync();
+
+                Assert.Equal(3, output.Count);
+                Assert.All(output, c => Assert.Equal(BlobComparisonType.Same, c.Type));
+                Assert.Equal(blobNames, output.Select(x => x.Left.Blob.Name).ToArray());
+                Assert.Equal(blobNames, output.Select(x => x.Right.Blob.Name).ToArray());
+            }
+        }
+
+        public class DetectsMissingFromRight : Test
+        {
+            public DetectsMissingFromRight(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0", "b", "1", "c", "2");
+                await CreateRightBlockBlobsAsync("a", "0", "c", "2");
+
+                var output = await target.ToListAsync();
+
+                Assert.Equal(3, output.Count);
+                Assert.Equal(BlobComparisonType.Same, output[0].Type);
+                Assert.Equal("a", output[0].Left.Blob.Name);
+                Assert.Equal("a", output[0].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.MissingFromRight, output[1].Type);
+                Assert.Equal("b", output[1].Left.Blob.Name);
+                Assert.Null(output[1].Right);
+                Assert.Equal(BlobComparisonType.Same, output[2].Type);
+                Assert.Equal("c", output[2].Left.Blob.Name);
+                Assert.Equal("c", output[2].Right.Blob.Name);
+            }
+        }
+
+        public class DetectsMissingFromLeft : Test
+        {
+            public DetectsMissingFromLeft(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0", "c", "2");
+                await CreateRightBlockBlobsAsync("a", "0", "b", "1", "c", "2");
+
+                var output = await target.ToListAsync();
+
+                Assert.Equal(3, output.Count);
+                Assert.Equal(BlobComparisonType.Same, output[0].Type);
+                Assert.Equal("a", output[0].Left.Blob.Name);
+                Assert.Equal("a", output[0].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.MissingFromLeft, output[1].Type);
+                Assert.Null(output[1].Left);
+                Assert.Equal("b", output[1].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.Same, output[2].Type);
+                Assert.Equal("c", output[2].Left.Blob.Name);
+                Assert.Equal("c", output[2].Right.Blob.Name);
+            }
+        }
+
+        public class DetectsMissingAndDifferentContent : Test
+        {
+            public DetectsMissingAndDifferentContent(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0-0", "c", "2", "d", "3");
+                await CreateRightBlockBlobsAsync("a", "0-1", "b", "1", "c", "2");
+
+                var output = await target.ToListAsync();
+
+                Assert.Equal(4, output.Count);
+                Assert.Equal(BlobComparisonType.DifferentContent, output[0].Type);
+                Assert.Equal("a", output[0].Left.Blob.Name);
+                Assert.Equal("a", output[0].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.MissingFromLeft, output[1].Type);
+                Assert.Null(output[1].Left);
+                Assert.Equal("b", output[1].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.Same, output[2].Type);
+                Assert.Equal("c", output[2].Left.Blob.Name);
+                Assert.Equal("c", output[2].Right.Blob.Name);
+                Assert.Equal(BlobComparisonType.MissingFromRight, output[3].Type);
+                Assert.Equal("d", output[3].Left.Blob.Name);
+                Assert.Null(output[3].Right);
+            }
+        }
+
+        public class DetectsMissingContentMD5 : Test
+        {
+            public DetectsMissingContentMD5(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0");
+                await CreateBlockBlobAsyncWithoutContentMD5(RightContainer, "a", "1");
+
+                var output = await target.ToListAsync();
+
+                var comparison = Assert.Single(output);
+                Assert.Equal(BlobComparisonType.MissingContentMD5, comparison.Type);
+                Assert.Equal("a", comparison.Left.Blob.Name);
+                Assert.Equal("a", comparison.Right.Blob.Name);
+            }
+        }
+
+        public class HandlesMissingContentMD5WhenSizeIsDifferent : Test
+        {
+            public HandlesMissingContentMD5WhenSizeIsDifferent(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0");
+                await CreateBlockBlobAsyncWithoutContentMD5(RightContainer, "a", "00");
+
+                var output = await target.ToListAsync();
+
+                var comparison = Assert.Single(output);
+                Assert.Equal(BlobComparisonType.DifferentContent, comparison.Type);
+                Assert.Equal("a", comparison.Left.Blob.Name);
+                Assert.Equal("a", comparison.Right.Blob.Name);
+            }
+        }
+
+        public class HandlesDifferentBlobType : Test
+        {
+            public HandlesDifferentBlobType(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateLeftBlockBlobsAsync("a", "0");
+                await CreateAppendBlobAsync(RightContainer, "a", "0");
+
+                var output = await target.ToListAsync();
+
+                var comparison = Assert.Single(output);
+                Assert.Equal(BlobComparisonType.DifferentBlobType, comparison.Type);
+                Assert.Equal("a", comparison.Left.Blob.Name);
+                Assert.Equal("a", comparison.Right.Blob.Name);
+            }
+        }
+
+        public class HandlesUnsupportedBlobType : Test
+        {
+            public HandlesUnsupportedBlobType(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task Run()
+            {
+                var target = Create();
+                await CreateAppendBlobAsync(LeftContainer, "a", "0");
+                await CreateAppendBlobAsync(RightContainer, "a", "0");
+
+                var output = await target.ToListAsync();
+
+                var comparison = Assert.Single(output);
+                Assert.Equal(BlobComparisonType.UnsupportedBlobType, comparison.Type);
+                Assert.Equal("a", comparison.Left.Blob.Name);
+                Assert.Equal("a", comparison.Right.Blob.Name);
+            }
+        }
+
         public class Test : BaseFacts, IAsyncLifetime
         {
             public Test(ITestOutputHelper output) : base(output)
