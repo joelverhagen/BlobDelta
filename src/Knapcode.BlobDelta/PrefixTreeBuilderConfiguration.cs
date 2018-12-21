77e20f5ef226e9b7b71b68e170164ad2c27215af
Joel Verhagen <joel.verhagen@gmail.com>
2018-12-20 20:40:12 -0800

    Add worker count configuration

--- src/Knapcode.BlobDelta/PrefixTreeBuilderConfiguration.cs
+++ src/Knapcode.BlobDelta/PrefixTreeBuilderConfiguration.cs
@@ -5,15 +5,22 @@ namespace Knapcode.BlobDelta
 {
     public class PrefixTreeBuilderConfiguration
     {
-        public PrefixTreeBuilderConfiguration() : this(LogLevel.Trace)
+        public PrefixTreeBuilderConfiguration() : this(16, LogLevel.Trace)
         {
         }
 
-        public PrefixTreeBuilderConfiguration(LogLevel minimumLogLevel)
+        public PrefixTreeBuilderConfiguration(int workerCount, LogLevel minimumLogLevel)
         {
+            WorkerCount = workerCount;
             MinimumLogLevel = minimumLogLevel;
+
+            if (workerCount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workerCount), "The worker counter must be at least 1.");
+            }
         }
 
+        public int WorkerCount { get; }
         public LogLevel MinimumLogLevel { get; }
     }
 }
